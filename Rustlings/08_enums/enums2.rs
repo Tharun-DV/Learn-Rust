2a657e805b5aa20ad1450f0d0ddc1351d5a3d4fa
Tharun D V <techinformationc@gmail.com>
2024-09-17 16:09:42 +0530

    Learned enum and strings

---
+++ Rustlings/08_enums/enums2.rs
@@ -0,0 +1,42 @@
+#![allow(dead_code)]
+
+use std::env::home_dir;
+
+#[derive(Debug)]
+struct Point {
+    x: u64,
+    y: u64,
+}
+
+#[derive(Debug)]
+enum Message {
+    // TODO: Define the different variants used below.
+    Resize{width:i32,height:i32},
+    Move(Point),
+    Echo(String),
+    ChangeColor(i32,i32,i32),
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{self:?}");
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Resize {
+            width: 10,
+            height: 30,
+        },
+        Message::Move(Point { x: 10, y: 15 }),
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
